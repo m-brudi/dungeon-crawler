ddcffcd8a61eec432b6e103bbfd1fd05f686c25f
Michał <brudermichal@gmail.com>
2020-04-05 11:17:41 +0200

    Small fixes in inventory system

--- Assets/Scripts/Spawn.cs
+++ Assets/Scripts/Spawn.cs
@@ -20,6 +20,7 @@ public class Spawn : MonoBehaviour
     }
     public void SpawnDroppedItem() {
         Vector2 playerPos = new Vector2(player.position.x, player.position.y+1);
+        //Debug.Log(item);
         Instantiate(item, playerPos, Quaternion.identity);
 
         //THROW WEAPON
