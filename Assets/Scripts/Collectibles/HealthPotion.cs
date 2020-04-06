546e0f424cf45a4d31ec5f2e5e45471aa5fcfc5f
Maks Leone <maksymilian.leone@smith-nephew.com>
2020-04-06 16:31:28 +0200

    Collectibles added - ML

---
+++ Assets/Scripts/Collectibles/HealthPotion.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthPotion : MonoBehaviour
+{
+    Health playerHealth;
+
+    void Awake()
+    {
+        playerHealth = FindObjectOfType<Health>();
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player") && playerHealth.health < playerHealth.numOfHearts)
+        {
+            Destroy(gameObject);
+            playerHealth.health++;
+        }
+    }
+
+}
---
+++ Assets/Scripts/Collectibles/HeartPotion.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeartPotion : MonoBehaviour
+{
+    Health playerHealth;
+
+    void Awake()
+    {
+        playerHealth = FindObjectOfType<Health>();
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player") && playerHealth.numOfHearts < 5)
+        {
+            Destroy(gameObject);
+            playerHealth.numOfHearts++;
+            playerHealth.health++;
+        }
+    }
+}
