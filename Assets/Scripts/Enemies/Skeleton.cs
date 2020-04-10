87e0ae8265cda08d1e8cdf44d94aacbec2171b1c
Michał <brudermichal@gmail.com>
2020-04-10 13:55:10 +0200

    Skeleton Fix and Update

--- Assets/Scripts/Enemies/Skeleton.cs
+++ Assets/Scripts/Enemies/Skeleton.cs
@@ -12,46 +12,74 @@ public class Skeleton: Enemy
     
     
     public GameObject player;
-    public float chaseRadius;
-    public Transform homePosition;
+    public float seeRadius;
+    public float ChaseRadius;
+    public Vector2 homePosition;
     public float moveSpeed;
 
     private Animator anim;
     private Rigidbody2D myRigidbody;
     private bool playerInRange = false;
     private bool walk = true;
-    private bool seenPlayer = false;
-
+    private float diff;
+    private bool doneWaiting = false;
+    private float actionRadius;
+    private float returnTime;
     void Start() {
         anim = GetComponent<Animator>();
         player = GameObject.FindWithTag("Player");
+        homePosition = new Vector2(transform.position.x, transform.position.y);
+        actionRadius = seeRadius;
+        
     }
 
     void FixedUpdate() {
-        float diff = player.transform.position.x - transform.position.x;
-        if (diff > 0) {
-
-            transform.localScale = new Vector3(1, 1, 1);
-        }
-        else {
-            transform.localScale = new Vector3(-1, 1, 1);
-        }
+        diff = player.transform.position.x - transform.position.x;
         CheckDistance();
     }
 
     void CheckDistance() {
-        if(Vector3.Distance(player.transform.position, transform.position) <= 0.8f) {
+        //if player in range for attack
+        if (Vector3.Distance(player.transform.position, transform.position) <= 0.8f) {
             walk = false;
         }
-        if (Vector3.Distance(player.transform.position, transform.position) <= chaseRadius && walk && !seenPlayer) {
-            seenPlayer = true;
+
+        //if player in chase radius
+        if (Vector3.Distance(player.transform.position, transform.position) <= actionRadius && walk) {
+
+            actionRadius = ChaseRadius;
+
+            if (diff > 0) {
+                transform.localScale = new Vector3(1, 1, 1);
+            } else {
+                transform.localScale = new Vector3(-1, 1, 1);
+            }
             transform.position = Vector3.MoveTowards(transform.position, player.transform.position, moveSpeed * Time.deltaTime);
             anim.SetTrigger("Walking");
-        }else if (seenPlayer && walk){
-            transform.position = Vector3.MoveTowards(transform.position, player.transform.position, moveSpeed * Time.deltaTime);
-            anim.SetTrigger("Walking");
-        }
-        else {
+            doneWaiting = false;
+            StopCoroutine(WaitForReturn());
+
+        //if player not in range and skeleton not close to home position
+        } else if (Vector3.Distance(transform.position, homePosition) > 2) {
+
+            StartCoroutine(WaitForReturn());
+
+            if (doneWaiting) {
+                if (homePosition.x - transform.position.x > 0) {
+                    transform.localScale = new Vector3(1, 1, 1);
+                } else {
+                    transform.localScale = new Vector3(-1, 1, 1);
+                }
+                transform.position = Vector3.MoveTowards(transform.position, homePosition, moveSpeed * Time.deltaTime);
+                walk = true;
+                anim.SetTrigger("Walking");
+            } else {
+                anim.SetTrigger("notWalking");
+            }
+            //is in home position
+        } else {
+            doneWaiting = false;
+            actionRadius = seeRadius;
             anim.SetTrigger("notWalking");
         }
     }
@@ -88,21 +116,30 @@ public class Skeleton: Enemy
     {
         yield return new WaitForSeconds(attackDelay);
         CameraMove.Shake((transform.position - mousePos).normalized, 5f, 0.1f);
-        Debug.Log("chuj");
         //damage the player
         if (playerInRange) {
             player.GetComponent<Health>().TakeDamage(damage);
             StartCoroutine(AttackAgain());
         }
     }
+
+    //Time to wait for next attack
     private IEnumerator AttackAgain() {
         yield return new WaitForSeconds(0.9f);
         attack();
     }
 
+    //Time to wait before goin back to homeposition
+    private IEnumerator WaitForReturn() {
+        returnTime = UnityEngine.Random.Range(3,5);
+        Debug.Log(returnTime);
+        yield return new WaitForSeconds(returnTime);
+        doneWaiting = true;
+    }
+
     private void OnDrawGizmosSelected() {
         Gizmos.color = Color.red;
-        Gizmos.DrawWireSphere(transform.position, chaseRadius);
+        Gizmos.DrawWireSphere(transform.position, actionRadius);
     }
 
 }
